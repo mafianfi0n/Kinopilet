5da78461d426d029a047de528523833a24450b2e
opilane <opilane@HP-CZC6297B9B>
2020-09-14 12:26:13 +0300

    Добавлены картинки в лейблы и проверки для корректной работы. Начата работа с БД

--- kinopiletid/Form1.Designer.cs
+++ kinopiletid/Form1.Designer.cs
@@ -59,7 +59,7 @@
             // 
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(383, 52);
+            this.pictureBox2.Location = new System.Drawing.Point(380, 52);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(162, 221);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
